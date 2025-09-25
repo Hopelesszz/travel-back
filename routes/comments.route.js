d9b4134d541fbd6e3d430d2822880fb434c02009
Egor <savitsky.egor318@gmail.com>
2025-09-25 10:58:47 +0300

    error fixing

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { addComment, updateComment, deleteComment, getCommentsByPost, getOneComment, getComments } from '../controllers/comments.controller.js';
-import { verifyUser } from '../utils/verifyToken.js';
+import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/getAllComments', getComments);
 router.get('/getOneComment/:id', getOneComment);
 router.get('/getCommentsByPost/:postId', getCommentsByPost);
-router.delete('/deleteComment/:id', verifyUser, deleteComment);
-router.put('/updateComment/:id', verifyUser, updateComment);
-router.post('/addComment', verifyUser, addComment);
+router.delete('/deleteComment/:id', verifyToken, deleteComment);
+router.put('/updateComment/:id', verifyToken, updateComment);
+router.post('/addComment', verifyToken, addComment);
 
 export default router;
