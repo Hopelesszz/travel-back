9b9b38767d2ce8ed673a67618f602deff927f594
Egor <savitsky.egor318@gmail.com>
2025-10-04 15:04:05 +0300

    Error fixing

--- routes/userAward.route.js
+++ routes/userAward.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { addUserAward,getOneUserAward,getUserAwards,updateUserAward,deleteUserAward } from '../controllers/userAward.controller.js';
+import { addUserAward,getOneUserAward,getUserAwards,updateUserAward,deleteUserAward,getUserAwardsByUser } from '../controllers/userAward.controller.js';
 import { verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/getAllUserAwards', getUserAwards);
 router.get('/getOneUserAward/:id', getOneUserAward);
+router.get('/getUserAwardsByUser/:userId', getUserAwardsByUser);
 router.delete('/deleteUserAward/:id', verifyUser, deleteUserAward);
 router.put('/updateUserAward/:id', verifyUser, updateUserAward);
 router.post('/addUserAward', verifyUser, addUserAward);
